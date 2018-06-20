58cf32a00611e2e001f0226aa08de7f1ea762bdc
PBJ <kkbbbj1@naver.com>
2018-06-20 20:57:09 +0900

    densenet complete but OOM

--- mydemosaicking_header.py
+++ mydemosaicking_header.py
@@ -19,10 +19,12 @@ import re
 import math
 import h5py
 
+
 def tf_log10(x):
     numerator = tf.log(x)
     denominator = tf.log(tf.constant(10, dtype=numerator.dtype)) #상용로그 정의
     return numerator / denominator
+
 
 def load_h5(directory, filename, num):
     dirname=directory + filename
@@ -39,83 +41,13 @@ def load_h5(directory, filename, num):
                 s = s+1
             h5 = np.array(hf.get(filename[s+1:-3]))
             h5 = np.reshape(h5,(1, h5.shape[0], h5.shape[1],1))    
-    return h5    
+    return h5  
+
 
 def PSNR(y_true, y_pred):
 	max_pixel = 1.0
 	return 10.0 * tf_log10((max_pixel ** 2) / (K.mean(K.square(y_pred - y_true)))) 
 
-####################################################################################################################
-
-def load_images(directory):
-	images = []
-	for root, dirnames, filenames in os.walk(directory):
-	    for filename in filenames:
-	        if re.search("\.(jpg|jpeg|png|bmp|tiff)$", filename):
-	            filepath = os.path.join(root, filename)
-	            image = ndimage.imread(filepath, mode="L")
-	            images.append(image)
-	            
-	images = np.array(images)
-	array_shape = np.append(images.shape[0:3], 1)
-	images = np.reshape(images, (array_shape))
-
-	return images
-
-def get_image_list(data_path):
-    l = glob.glob(os.path.join(data_path,"*"))
-    # print(len(l))
-    l = [f for f in l if re.search("^\d+_\d+_gt.mat$", os.path.basename(f))]
-    # print(len(l))
-    r_train_list = []
-    g_train_list = []
-    b_train_list = []
-    string = '.mat'
-    count=0
-    for f in l:        
-        if os.path.exists(f):            
-            if count%3 == 0:
-                r_train_list.append([f, f[:-7]+string])
-                count=count+1
-            elif count%3 == 1:
-                g_train_list.append([f, f[:-7]+string])
-                count=count+1
-            elif count%3 == 2:
-                b_train_list.append([f, f[:-7]+string])
-                count=count+1
-    return r_train_list, g_train_list, b_train_list
-
-#def get_valid_image
-
-def get_image_batch(train_list, offset, BATCH_SIZE):
-    target_list = train_list[offset:offset+BATCH_SIZE]
-    input_list = []
-    gt_list = []
-    for pair in target_list:
-        input_img = scipy.io.loadmat(pair[1])['patch']
-        gt_img = scipy.io.loadmat(pair[0])['patch']        
-        input_list.append(input_img)
-        gt_list.append(gt_img)
-    input_list = np.array(input_list)
-    input_list.resize([BATCH_SIZE, IMG_SIZE[0], IMG_SIZE[1], 1])
-    gt_list = np.array(gt_list)
-    gt_list.resize([BATCH_SIZE, IMG_SIZE[0], IMG_SIZE[1], 1])
-    return input_list, gt_list
-
-def test_get_image_batch(train_list, offset, BATCH_SIZE):
-	target_list = train_list[offset:offset+BATCH_SIZE]
-	input_list = []
-	gt_list = []
-	for pair in target_list:
-		input_img = scipy.io.loadmat(pair[1])['patch']
-		gt_img = scipy.io.loadmat(pair[0])['patch']
-		input_list.append(input_img)
-		gt_list.append(gt_img)
-	input_list = np.array(input_list)
-	input_list.resize([BATCH_SIZE, input_img.shape[0], input_img.shape[1], 1])
-	gt_list = np.array(gt_list)
-	gt_list.resize([BATCH_SIZE, input_img.shape[0], input_img.shape[1], 1])
-	return input_list, gt_list
 
 class threadsafe_iter:
     """Takes an iterator/generator and makes it thread-safe by
@@ -140,22 +72,27 @@ def threadsafe_generator(f):
         return threadsafe_iter(f(*a, **kw))
     return g
 
+
 @threadsafe_generator
-def image_gen(target_list, BATCH_SIZE):
-	while True:
-		for step in range(len(target_list)//BATCH_SIZE):
-			offset = step*BATCH_SIZE
-			batch_x, batch_y = get_image_batch(target_list, offset, BATCH_SIZE)
-			yield (batch_x, batch_y)
+def train_image_gen(patch, gt_patch, channel, BATCH_SIZE):
+    patch = np.array(patch[channel])
+    gt_patch = np.array(gt_patch[channel])
+    while True:
+        for step in range(len(patch)//BATCH_SIZE):
+            offset = step*BATCH_SIZE
+            batch_x = patch[offset:offset+BATCH_SIZE]/255
+            batch_y = gt_patch[offset:offset+BATCH_SIZE]/255
+            yield (batch_x, batch_y)
             
-def test_image_gen(target_list, BATCH_SIZE):
-	while True:
-		for step in range(len(target_list)//BATCH_SIZE):
-			offset = step*BATCH_SIZE
-			batch_x, batch_y = test_get_image_batch(target_list, offset, BATCH_SIZE)
-			yield (batch_x, batch_y)
+            
+def valid_image_gen(img, gt_img, num):
+    while num>0:
+        batch_x = np.array(img[num-1])/255
+        batch_y = np.array(gt_img[num-1])/255
+        num = num-1
+        yield (batch_x, batch_y)
+        
 
 def step_decay(epoch):
     decay_rate = LEARNING_RATE/ (np.power(10,np.floor(epoch/20)))
     return decay_rate
-
---
+++ mydensenet.py
@@ -0,0 +1,146 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from keras.models import Model
+from keras.layers import Input, add
+from keras.layers.merge import concatenate
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.convolutional import Conv2D
+import keras.backend as K
+
+def DenseNet(nb_dense_block=4, growth_rate=32, nb_filter=64, reduction=0.0, dropout_rate=0.0, weights_path=None):
+    '''Instantiate the DenseNet 121 architecture,
+        # Arguments
+            nb_dense_block: number of dense blocks to add to end
+            growth_rate: number of filters to add per dense block
+            nb_filter: initial number of filters
+            reduction: reduction factor of transition blocks.
+            dropout_rate: dropout rate            
+            weights_path: path to pre-trained weights
+        # Returns
+            A Keras model instance.
+    '''
+    
+    # compute compression factor
+    compression = 1.0 - reduction
+
+    
+    img_input = Input(shape=(None, None, 1), name='data')
+    
+
+    # From architecture for ImageNet (Table 1 in the paper)
+    nb_filter = 64
+    nb_layers = [6,6,6,6] # For DenseNet-121
+
+    # Initial convolution
+
+    x = Conv2D(nb_filter, (3, 3), padding='same', kernel_initializer='glorot_normal', name='conv1')(img_input)
+    x = Activation('relu', name='relu1')(x)
+
+
+    # Add dense blocks
+    for block_idx in range(nb_dense_block - 1):
+        stage = block_idx+2
+        LR = x
+        x, nb_filter = dense_block(x, stage, nb_layers[block_idx], nb_filter, growth_rate, dropout_rate=dropout_rate)
+        
+        # Add transition_block
+        x = transition_block(x, stage, nb_filter, compression=compression, dropout_rate=dropout_rate)
+        #x = add([LR, x])
+        nb_filter = int(nb_filter * compression)
+
+    final_stage = stage + 1
+    x, nb_filter = dense_block(x, final_stage, nb_layers[-1], nb_filter, growth_rate, dropout_rate=dropout_rate)
+    x = Conv2D(1, (3, 3), padding='same', kernel_initializer='glorot_normal')(x)
+    x = Activation('relu', name='relu'+str(final_stage)+'_blk')(x)
+    x = add([x, img_input])
+
+    model = Model(img_input, x, name='densenet')
+
+    if weights_path is not None:
+        model.load_weights(weights_path)
+
+    return model
+
+
+def conv_block(x, stage, branch, nb_filter, dropout_rate=None):
+    '''Apply BatchNorm, Relu, bottleneck 1x1 Conv2D, 3x3 Conv2D, and option dropout
+        # Arguments
+            x: input tensor 
+            stage: index for dense block
+            branch: layer index within each dense block
+            nb_filter: number of filters
+            dropout_rate: dropout rate            
+    '''
+    conv_name_base = 'conv' + str(stage) + '_' + str(branch)
+    relu_name_base = 'relu' + str(stage) + '_' + str(branch)
+
+    # 1x1 Convolution (Bottleneck layer)
+    inter_channel = nb_filter * 4  
+    x = Conv2D(inter_channel, (1, 1), padding='same', kernel_initializer='glorot_normal', name=conv_name_base+'_x1')(x)
+    x = Activation('relu', name=relu_name_base+'_x1')(x)    
+
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+
+    # 3x3 Convolution
+    x = Conv2D(nb_filter, (3, 3), padding='same', kernel_initializer='glorot_normal', name=conv_name_base+'_x2')(x)
+    x = Activation('relu', name=relu_name_base+'_x2')(x)    
+    
+
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+
+    return x
+
+
+def transition_block(x, stage, nb_filter, compression=1.0, dropout_rate=None):
+    ''' Apply BatchNorm, 1x1 Convolution, averagePooling, optional compression, dropout 
+        # Arguments
+            x: input tensor
+            stage: index for dense block
+            nb_filter: number of filters
+            compression: calculated as 1 - reduction. Reduces the number of feature maps in the transition block.
+            dropout_rate: dropout rate            
+    '''
+
+    
+    conv_name_base = 'conv' + str(stage) + '_blk'
+    relu_name_base = 'relu' + str(stage) + '_blk'
+    
+    x = Conv2D(int(nb_filter * compression), (1, 1), padding='same', kernel_initializer='glorot_normal', name=conv_name_base)(x)
+    x = Activation('relu', name=relu_name_base)(x)
+    
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+  
+    return x
+
+
+def dense_block(x, stage, nb_layers, nb_filter, growth_rate, dropout_rate=None, grow_nb_filters=True):
+    ''' Build a dense_block where the output of each conv_block is fed to subsequent ones
+        # Arguments
+            x: input tensor
+            stage: index for dense block
+            nb_layers: the number of layers of conv_block to append to the model.
+            nb_filter: number of filters
+            growth_rate: growth rate
+            dropout_rate: dropout rate            
+            grow_nb_filters: flag to decide to allow number of filters to grow
+    '''
+
+    
+    concat_feat = x
+
+    for i in range(nb_layers):
+        branch = i+1
+        x = conv_block(concat_feat, stage, branch, growth_rate, dropout_rate)
+        concat_feat = concatenate([concat_feat, x], name='concat_'+str(stage)+'_'+str(branch))
+
+        if grow_nb_filters:
+            nb_filter += growth_rate
+
+    return concat_feat, nb_filter
+
